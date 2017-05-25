be155893beca39753b289aab8acca80bd06c2ecb
luofei <13632323692@139.com>
2017-05-25 20:24:57 +0800

    helloworld

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,12 @@
+// hello_world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello, world!");
+	return 0;
+}
+
